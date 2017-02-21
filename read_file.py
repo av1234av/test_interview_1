a51517b9462876055c0e6c243f9cb475e31c5618
Amit Virmani <amit_virmani@hotmail.com>
2017-02-20 22:56:50 -0500

    added more data

--- read_file.py
+++ read_file.py
@@ -9,12 +9,13 @@ class AssetRiskReport(object):
     def __init__(self, path):
         self._path = path
         self._position = dict()
+        self._tradeVol=defaultdict(int)
         self._prices = defaultdict(float)
 
     def _calc_position(self, trade):
-        pos = self._position.get(trade.symbol, {'Position': 0, 'TradeVol': 0, 'MktVal':0.0})
+        pos = self._position.get(trade.symbol, {'Position': 0, 'MktVal':0.0})
         pos['Position'] += trade.qty * (1 if trade.side == 'BUY' else -1)
-        pos['TradeVol'] += trade.qty
+        self._tradeVol[trade.counterParty] += trade.qty
         # pos['MktVal'] = pos['self._position'] * trade.price
         self._position[trade.symbol] = pos
 
@@ -53,11 +54,11 @@ class AssetRiskReport(object):
             print item[0], item[1]['Position'], item[1]['MktVal']
 
         print '----------- Trade Volume Report ------------'
-        for item in sorted(self._position.items(), key=lambda x: x[1]['TradeVol'], reverse=True)[:20]:
-            print item[0], item[1]['TradeVol']
+        for item in sorted(self._tradeVol.items(), key=lambda x: x['TradeVol'], reverse=True)[:20]:
+            print item[0], item[1]
 
 if __name__ == '__main__':
-    path='C:\\Users\\amit_\\PycharmProjects\\asset_risk\\Data'
+    path='C:\\Temp\\test_interview_1\\Data'
     asset_risk_report = AssetRiskReport(path)
     asset_risk_report.read_and_process()
     asset_risk_report.generate_report()
